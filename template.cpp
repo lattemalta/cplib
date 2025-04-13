ace9b6a1eda8b389ab94a100aa2f2ca3663b26ff
latte <latte.malta@gmail.com>
2025-04-14 02:44:50 +0900

    print

--- template.cpp
+++ template.cpp
@@ -28,6 +28,41 @@ std::ostream& operator<<(std::ostream& ost, const std::vector<T>& v) {
 
 inline int32_t parity(uint64_t x) { return __builtin_parity(x); }
 
+void print() {}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+    std::cout << head;
+    if (sizeof...(Tail)) {
+        std::cout << " ";
+    }
+    print(std::forward<Tail>(tail)...);
+}
+
+template <class... Args>
+void println(Args&&... args) {
+    print(args..., '\n');
+}
+
+#define SHOW(...)                                             \
+    SHOW_IMPL(__VA_ARGS__, SHOW6, SHOW5, SHOW4, SHOW3, SHOW2, \
+              SHOW1)(__VA_ARGS__)
+#define SHOW_IMPL(_1, _2, _3, _4, _5, _6, NAME, ...) NAME
+#define SHOW1(x) println(#x, "=", (x)), flush()
+#define SHOW2(x, y) println(#x, "=", (x), #y, "=", (y)), flush()
+#define SHOW3(x, y, z) \
+    println(#x, "=", (x), #y, "=", (y), #z, "=", (z)), flush()
+#define SHOW4(x, y, z, w) \
+    println(#x, "=", (x), #y, "=", (y), #z, "=", (z), #w, "=", (w)), flush()
+#define SHOW5(x, y, z, w, v)                                                 \
+    println(#x, "=", (x), #y, "=", (y), #z, "=", (z), #w, "=", (w), #v, "=", \
+            (v)),                                                            \
+        flush()
+#define SHOW6(x, y, z, w, v, u)                                              \
+    println(#x, "=", (x), #y, "=", (y), #z, "=", (z), #w, "=", (w), #v, "=", \
+            (v), #u, "=", (u)),                                              \
+        flush()
+
 #define int long long
 
 #define eb emplace_back
