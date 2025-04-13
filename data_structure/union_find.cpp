303e1ed5f38ca5a6a5ea5dd5fc0a2dae658f29e6
latte <latte.malta@gmail.com>
2025-04-13 18:25:21 +0900

    union_find_tree

---
+++ data_structure/union_find.cpp
@@ -0,0 +1,21 @@
+#include <cstdint>
+#include <vector>
+
+struct UnionFindTree {
+    std::vector<int32_t> data;
+    UnionFindTree(int32_t n) : data(n, -1) {}
+
+    int32_t find(const int32_t x) {
+        if (data[x] < 0) return x;
+        return data[x] = find(data[x]);
+    }
+    bool unite(int32_t x, int32_t y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return false;
+        if (data[x] > data[y]) std::swap(x, y);
+        data[x] += data[y];
+        data[y] = x;
+        return true;
+    }
+};
