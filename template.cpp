d6a089eb005f4cbfa02a1b2ecf401667d46c9c76
latte <latte.malta@gmail.com>
2025-04-13 18:27:41 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,16 +1,5 @@
 #include <bits/stdc++.h>
 
-#define int long long
-
-#define eb emplace_back
-#define all(v) (v).begin(), (v).end()
-#define fi first
-#define se second
-
-using vint = std::vector<int>;
-using pint = std::pair<int, int>;
-using vpint = std::vector<pint>;
-
 template <typename A, typename B>
 inline void chmin(A& a, B b) {
     if (a > b) a = b;
@@ -37,14 +26,22 @@ std::ostream& operator<<(std::ostream& ost, const std::vector<T>& v) {
     return ost;
 }
 
-inline int topbit(unsigned long long x) {
-    return x ? 63 - __builtin_clzll(x) : -1;
-}
+inline int32_t topbit(uint64_t x) { return x ? 63 - __builtin_clzll(x) : -1; }
 
-inline int popcount(unsigned long long x) { return __builtin_popcountll(x); }
+inline int32_t popcount(uint64_t x) { return __builtin_popcountll(x); }
 
-inline int parity(unsigned long long x) { return __builtin_parity(x); }
+inline int32_t parity(uint64_t x) { return __builtin_parity(x); }
 
+#define int long long
+
+#define eb emplace_back
+#define all(v) (v).begin(), (v).end()
+#define fi first
+#define se second
+
+using vint = std::vector<int>;
+using pint = std::pair<int, int>;
+using vpint = std::vector<pint>;
 using std::abs, std::pow, std::sqrt;
 using std::array, std::vector, std::string, std::queue, std::deque;
 using std::cin, std::cout, std::cerr, std::endl;
