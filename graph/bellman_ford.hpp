8db069136c8222dbf2e4c714769fc4ab3f0ce3da
latte <latte.malta@gmail.com>
2025-05-04 04:15:57 +0900

    bellman_ford

--- graph/bellman_ford.hpp
+++ graph/bellman_ford.hpp
@@ -1,9 +1,32 @@
+#pragma once
+
 #include <cstdint>
+#include <limits>
 #include <vector>
 
 #include "graph/graph.hpp"
 
-template <typename T, typename GT>
-std::vector<T> bellman_ford(const GT& g, int s) {
-    int V = g.
+template <typename T, bool directed>
+std::vector<T> bellman_ford(const Graph<T, directed>& G, const int32_t s) {
+    int V = G.size();
+    constexpr T INF = std::numeric_limits<T>::max();
+
+    std::vector<T> dist(V, INF);
+    dist[s] = 0;
+
+    for (int32_t t = 0; t < V - 1; t++) {
+        for (int32_t v = 0; v < V; v++) {
+            for (auto& e : G[v]) {
+                if (dist[e.from] == INF) continue;
+                dist[e.to] = std::min(dist[e.to], dist[e.from] + e.cost);
+            }
+        }
+    }
+    for (int32_t v = 0; v < V; v++) {
+        for (auto& e : G[v]) {
+            if (dist[e.from] == INF) continue;
+            if (dist[e.to] > dist[e.from] + e.cost) return {};
+        }
+    }
+    return dist;
 }
