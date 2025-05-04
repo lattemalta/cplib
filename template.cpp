1729a821084fc31938d9dc4d33cc75de78b8ee0a
latte <latte.malta@gmail.com>
2025-05-04 14:09:15 +0900

    update  template(rep)

--- template.cpp
+++ template.cpp
@@ -69,17 +69,29 @@ void println(Args&&... args) {
     println(#x, "=", (x), #y, "=", (y), #z, "=", (z), #w, "=", (w), #v, "=", \
             (v), #u, "=", (u))
 
+#define FOR1(a) for (int64_t _ = 0; _ < int64_t(a); ++_)
+#define FOR2(i, a) for (int64_t i = 0; i < int64_t(a); ++i)
+#define FOR3(i, a, b) for (int64_t i = a; i < int64_t(b); ++i)
+#define FOR4(i, a, b, c) for (int64_t i = a; i < int64_t(b); i += (c))
+#define FOR1_R(a) for (int64_t i = (a) - 1; i >= int64_t(0); --i)
+#define FOR2_R(i, a) for (int64_t i = (a) - 1; i >= int64_t(0); --i)
+#define FOR3_R(i, a, b) for (int64_t i = (b) - 1; i >= int64_t(a); --i)
+#define overload4(a, b, c, d, e, ...) e
+#define overload3(a, b, c, d, ...) d
+#define rep(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define rep_r(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
+
 #define eb emplace_back
 #define all(v) (v).begin(), (v).end()
 #define fi first
 #define se second
-#define int long long
+
+#define int long long  // <= 64bit ミ(°_°)ミ
+
 using vint = std::vector<int>;
 using pint = std::pair<int, int>;
 using vpint = std::vector<pint>;
 using namespace std;
-
-#define rep(i, n) for (int i = 0; i < (n); i++)
 
 constexpr int INF = 1ll << 61;
 
