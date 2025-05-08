b4b1acc7b7b9ceecad93b11a599771c5a9a2f3ec
latte <latte.malta@gmail.com>
2025-05-08 18:40:15 +0900

    linear_function

---
+++ algebra/monoid/linear_function.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <utility>
+
+template <typename T>
+struct LinearFunction {
+    using value_type = std::pair<T, T>;
+
+    // g(f(x))
+    static constexpr value_type op(const value_type& f,
+                                   const value_type& g) noexcept {
+        return {f.first * g.second + g.first, f.second * g.second};
+    }
+    static constexpr value_type e() noexcept { return {T(0), T(1)}; }
+    static constexpr bool commutative = false;
+    static constexpr bool invertible = false;
+};
