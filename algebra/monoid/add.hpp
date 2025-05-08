da2e4d02eb74e9c488d9e1d7f862c40b0b31a0f0
latte <latte.malta@gmail.com>
2025-05-08 16:30:32 +0900

    monoid add

---
+++ algebra/monoid/add.hpp
@@ -0,0 +1,11 @@
+template <typename X>
+struct Monoid_Add {
+    using value_type = X;
+    static constexpr X op(const X& x, const X& y) noexcept { return x + y; }
+    static constexpr X inv(const X& x) noexcept { return -x; }
+    static constexpr X pow(const X& x, const int64_t n) noexcept {
+        return X(n) * x;
+    }
+    static constexpr X e() noexcept { return X(0); }
+    static constexpr bool commutative = true;
+};
