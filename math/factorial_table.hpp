f9b827554089bf2e3ac2b84306950193532efcbe
latte <latte.malta@gmail.com>
2025-05-08 14:13:57 +0900

    update factrial_table

--- math/factorial_table.hpp
+++ math/factorial_table.hpp
@@ -4,18 +4,27 @@
 #include <vector>
 template <typename mint_t>
 struct FactorialTable {
-    std::vector<mint_t> fact, ifact, inv;
-    FactorialTable(const int64_t n) : fact(n), ifact(n), inv(n) {
-        inv[1] = 1;
+    FactorialTable(const int64_t n) : fact_(n), ifact_(n), inv_(n) {
+        inv_[1] = 1;
         constexpr auto mod = mint_t::get_mod();
         for (int32_t i = 2; i < n; i++) {
-            inv[i] = -inv[mod % i] * (mod / i);
+            inv_[i] = -inv_[mod % i] * (mod / i);
         }
-        fact[0] = 1;
-        ifact[0] = 1;
+        fact_[0] = 1;
+        ifact_[0] = 1;
         for (int i = 1; i < n; i++) {
-            fact[i] = fact[i - 1] * i;
-            ifact[i] = ifact[i - 1] * inv[i];
+            fact_[i] = fact_[i - 1] * i;
+            ifact_[i] = ifact_[i - 1] * inv_[i];
         }
     }
+    mint_t fact(const int64_t n) const { return fact_[n]; }
+    mint_t ifact(const int64_t n) const { return ifact_[n]; }
+    mint_t inv(const int64_t n) const { return inv_[n]; }
+    mint_t binom(const int64_t n, const int64_t k) const {
+        if (n < k || k < 0) return 0;
+        return fact_[n] * ifact_[k] * ifact_[n - k];
+    }
+
+   private:
+    std::vector<mint_t> fact_, ifact_, inv_;
 };
