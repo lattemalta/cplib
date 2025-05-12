8c6f6ad354a803886358369f67f2d07d61a81c72
latte0119 <latte.malta@gmail.com>
2025-05-12 17:44:08 +0000

    update lazy_segment_tree

--- data_structure/lazy_segment_tree.hpp
+++ data_structure/lazy_segment_tree.hpp
@@ -3,143 +3,101 @@
 #include <bit>
 #include <vector>
 
-// https://ei1333.github.io/library/structure/segment-tree/lazy-segment-tree.hpp.html
-
-template <typename S2, typename Op, typename E, typename F2, typename Act,
-          typename Composition, typename Id>
-struct LambdaActedMonoid {
-    using S = S2;
-    using F = F2;
-
-    S op(const S &a, const S &b) const { return _op(a, b); }
-
-    S e() const { return _e(); }
-
-    S act(const S &x, const F &f) const { return _act(x, f); }
-
-    F composition(const F &f, const F &g) const { return _composition(f, g); }
-
-    F id() const { return _id(); }
-
-    LambdaActedMonoid(Op _op, E _e, Act _act, Composition _composition, Id _id)
-        : _op(_op), _e(_e), _act(_act), _composition(_composition), _id(_id) {}
-
-   private:
-    Op _op;
-
-    E _e;
-
-    Act _act;
-
-    Composition _composition;
-
-    Id _id;
-};
-
-// Deduction guide for LambdaActedMonoid
-// This allows the compiler to deduce the template parameters of
-// LambdaActedMonoid based on the types of the arguments passed to its
-// constructor. It simplifies the instantiation of LambdaActedMonoid by avoiding
-// the need to explicitly specify the template arguments.
-template <typename Op, typename E, typename Act, typename Composition,
-          typename Id>
-LambdaActedMonoid(Op _op, E _e, Act _act, Composition _composition, Id _id)
-    -> LambdaActedMonoid<decltype(_e()), Op, E, decltype(_id()), Act,
-                         Composition, Id>;
-
 template <typename ActedMonoid>
 struct LazySegmentTree {
-    using S = typename ActedMonoid::S;
-    using F = typename ActedMonoid::F;
+    using MX = typename ActedMonoid::Monoid_X;
+    using MA = typename ActedMonoid::Monoid_A;
+    using X = typename MX::value_type;
+    using A = typename MA::value_type;
 
    private:
-    ActedMonoid m;
     int32_t n, sz, height;
-    std::vector<S> data;
-    std::vector<F> lazy;
+    std::vector<X> data;
+    std::vector<A> lazy;
 
     void update(const int32_t k) {
-        data[k] = m.op(data[2 * k], data[2 * k + 1]);
+        data[k] = MX::op(data[2 * k], data[2 * k + 1]);
     }
 
-    void all_apply(const int32_t k, const F &x) {
-        data[k] = m.act(data[k], x);
-        lazy[k] = m.composition(x, lazy[k]);
+    void all_apply(const int32_t k, const int32_t len, const A &a) {
+        data[k] = ActedMonoid::act(data[k], len, a);
+        lazy[k] = MA::op(a, lazy[k]);
     }
 
-    void propagate(const int32_t k) {
-        if (lazy[k] == m.id()) return;
-        all_apply(2 * k, lazy[k]);
-        all_apply(2 * k + 1, lazy[k]);
-        lazy[k] = m.id();
+    void propagate(const int32_t k, const int32_t len) {
+        if (lazy[k] == MA::e()) return;
+        all_apply(2 * k, len >> 1, lazy[k]);
+        all_apply(2 * k + 1, len >> 1, lazy[k]);
+        lazy[k] = MA::e();
     }
 
    public:
-    LazySegmentTree(ActedMonoid m, int32_t n) : m(m), n(n) {
+    LazySegmentTree(int32_t n) : n(n) {
         sz = std::bit_ceil(static_cast<uint32_t>(n));
         height = std::countr_zero(static_cast<uint32_t>(sz));
-        data.assign(2 * sz, m.e());
-        lazy.assign(2 * sz, m.id());
+        data.assign(2 * sz, MX::e());
+        lazy.assign(2 * sz, MA::e());
     }
 
-    void build(const std::vector<S> &v) {
+    void build(const std::vector<X> &v) {
         assert(v.size() <= n);
         for (int32_t i = 0; i < v.size(); ++i) data[sz + i] = v[i];
         for (int32_t i = sz - 1; i > 0; --i) update(i);
     }
 
-    void set(int32_t k, const S &x) {
+    void set(int32_t k, const X &x) {
         k += sz;
-        for (int32_t i = height; i > 0; --i) propagate(k >> i);
+        for (int32_t i = height; i > 0; --i) propagate(k >> i, 1 << i);
         data[k] = x;
         for (int32_t i = 1; i <= height; ++i) update(k >> i);
     }
 
-    S get(int32_t k) {
+    X get(int32_t k) {
         k += sz;
-        for (int32_t i = height; i > 0; --i) propagate(k >> i);
+        for (int32_t i = height; i > 0; --i) propagate(k >> i, 1 << i);
         return data[k];
     }
 
-    S prod(int32_t l, int32_t r) {
-        if (l >= r) return m.e();
+    X prod(int32_t l, int32_t r) {
+        if (l >= r) return MX::e();
         l += sz;
         r += sz;
         for (int32_t i = height; i > 0; --i) {
-            if (((l >> i) << i) != l) propagate(l >> i);
-            if (((r >> i) << i) != r) propagate((r - 1) >> i);
+            if (((l >> i) << i) != l) propagate(l >> i, 1 << i);
+            if (((r >> i) << i) != r) propagate((r - 1) >> i, 1 << i);
         }
-        S res_l = m.e(), res_r = m.e();
+        X res_l = MX::e(), res_r = MX::e();
         while (l < r) {
-            if (l & 1) res_l = m.op(res_l, data[l++]);
-            if (r & 1) res_r = m.op(data[--r], res_r);
+            if (l & 1) res_l = MX::op(res_l, data[l++]);
+            if (r & 1) res_r = MX::op(data[--r], res_r);
             l >>= 1;
             r >>= 1;
         }
-        return m.op(res_l, res_r);
+        return MX::op(res_l, res_r);
     }
 
-    void apply(int32_t k, const F &x) {
+    void apply(int32_t k, const A &a) {
         k += sz;
-        for (int32_t i = height; i > 0; --i) propagate(k >> i);
-        all_apply(k, x);
+        for (int32_t i = height; i > 0; --i) propagate(k >> i, 1 << i);
+        all_apply(k, 1, a);
         for (int32_t i = 1; i <= height; ++i) update(k >> i);
     }
 
-    void range_apply(int32_t l, int32_t r, const F &x) {
+    void range_apply(int32_t l, int32_t r, const A &a) {
         if (l >= r) return;
         l += sz;
         r += sz;
         for (int32_t i = height; i > 0; --i) {
-            if (((l >> i) << i) != l) propagate(l >> i);
-            if (((r >> i) << i) != r) propagate((r - 1) >> i);
+            if (((l >> i) << i) != l) propagate(l >> i, 1 << i);
+            if (((r >> i) << i) != r) propagate((r - 1) >> i, 1 << i);
         }
-        int32_t l2 = l, r2 = r;
+        int32_t l2 = l, r2 = r, len = 1;
         while (l < r) {
-            if (l & 1) all_apply(l++, x);
-            if (r & 1) all_apply(--r, x);
+            if (l & 1) all_apply(l++, len, a);
+            if (r & 1) all_apply(--r, len, a);
             l >>= 1;
             r >>= 1;
+            len <<= 1;
         }
         l = l2, r = r2;
         for (int32_t i = 1; i <= height; ++i) {
