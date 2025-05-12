6934976f5421aacc7d2abcd1730726e49f28449d
latte0119 <latte.malta@gmail.com>
2025-05-12 18:11:01 +0000

    add pow

--- algebra/monoid/add.hpp
+++ algebra/monoid/add.hpp
@@ -5,6 +5,9 @@ struct AddMonoid {
     using value_type = X;
     static constexpr X op(const X& x, const X& y) noexcept { return x + y; }
     static constexpr X inv(const X& x) noexcept { return -x; }
+    static constexpr X pow(const X& x, const int64_t exp) noexcept {
+        return x * exp;
+    }
     static constexpr X e() noexcept { return X(0); }
     static constexpr bool commutative = true;
     static constexpr bool invertible = true;
